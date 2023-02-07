9e8bc849e40f47d68db7fd7a02fa2202af7b7222
buihien0109 <buihien01091997@gmail.com>
2023-02-07 19:41:54 +0700

    update

---
+++ day_04/ul-li/main.js
@@ -0,0 +1,37 @@
+// Thêm 1 nút “add” + 1 ô input để nhập text. Mỗi khi bấm nút thêm 1 thẻ <li> có nội dung là nội dung trong ô input vào cuối danh sách
+const listEl = document.getElementById("list");
+
+// Tạo thẻ input
+const inputEl = document.createElement("input");
+inputEl.type = "text";
+inputEl.placeholder = "Enter text ...";
+listEl.insertAdjacentElement("beforebegin", inputEl);
+
+// Tạo button
+const btnAdd = document.createElement("button");
+btnAdd.innerText = "Add";
+inputEl.insertAdjacentElement("afterend", btnAdd);
+
+const addElement = () => {
+    const value = inputEl.value;
+    if(value.trim() === "") {
+        alert("Nội dung không được để trống");
+        return;
+    }
+
+    listEl.insertAdjacentHTML("beforeend", `<li>${value}</li>`);
+    inputEl.value = "";
+}
+
+// Add bằng cách bấm nút
+// btnAdd.addEventListener("click", addElement)
+btnAdd.onclick = addElement
+
+// Add bằng phím Enter
+inputEl.addEventListener("keydown", (e) => {
+    if(e.key === "Enter") {
+        addElement();
+    }
+})
+
+// 7. Thêm 1 nút “remove”. Chức năng để xóa thẻ <li> cuối cùng của danh sách
