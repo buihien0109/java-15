36421eab2cdab39390407e0b8bb98729de1a1347
buihien0109 <buihien01091997@gmail.com>
2023-02-16 20:04:26 +0700

    update

---
+++ day_06/seconda-app/src/main/java/com/example/secondaapp/SecondaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondaAppApplication.class, args);
+	}
+
+}
